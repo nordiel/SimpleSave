f32ed21bc987d59414890296943f8171c1fde74c
EkkoCTRL <non@none.none>
2023-04-27 02:12:14 -0400

    Release v1.0 (Used for Presentation)

--- SimpleSavePM.cpp
+++ SimpleSavePM.cpp
@@ -1,87 +1,60 @@
 
-#include <iostream> // input output stream library
-#include <string> // library for string manipulation
-#include <cmath> // libary for math functions
-#include <vector> // library for poitnters and vectors
-#include <fstream> // library for file manipulation
-#include <sstream>
-#include <chrono> 
-#include <thread>
+#include <iostream> // Library for input/output stream
+#include <string> // Library for string manipulation
+#include <vector> // Library for poitnters and vectors
+#include <fstream> // Library for file manipulation
+#include <chrono> // Library for time measurement and manipulation
+#include <thread> // Library for multi-threading
+#include <cstdio> // Library for data stream manipulation
 
-// namespace standard
 using namespace std;
 
-// functions
+// Functions
 
-void encryptFile(const string& inputFilename, const string& outputFilename, int encryptionKey) {
+// Exit screen
+void exitScreen()
+{
+	system("cls");
+	cout << R"(
 
-	// Open the input file
-	ifstream inputFile(inputFilename);
+       ____                 _ ____                  
+      / ___| ___   ___   __| | __ ) _   _  ___      
+     | |  _ / _ \ / _ \ / _` |  _ \| | | |/ _ \     
+     | |_| | (_) | (_) | (_| | |_) | |_| |  __/     
+      \____|\___/ \___/ \__,_|____/ \__, |\___|     
+                                    |___/           
 
-	if (!inputFile) {
-
-		cerr << "Error opening input file." << endl;
-
-		return;
-	}
-
-	// Open the output file
-	ofstream outputFile(outputFilename);
-
-	if (!outputFile) {
-
-		cerr << "Error opening output file." << endl;
-
-		return;
-
-	}
-
-	// Encrypt the contents of the input file
-	char c;
-	while (inputFile.get(c)) {
-
-		c += encryptionKey; // Add the encryption key to the ASCII code of each character
-
-		outputFile.put(c); // Write the encrypted character to the output file
-
-	}
-
-	// Close the files
-	inputFile.close();
-	outputFile.close();
-
-	cout << "File encrypted successfully." << endl;
+  
+)";
 }
 
-void decryptFile(const string& inputFilename, const string& outputFilename, int encryptionKey) {
-
+//Function to decrypt the bundle
+void decryptFile(const string& encryptedBundleFile)
+{
 	// Open the input file
-	ifstream inputFile(inputFilename);
+	ifstream inputFile(encryptedBundleFile);
 
-	if (!inputFile) {
-
+	if (!inputFile)
+	{
 		cerr << "Error opening input file." << endl;
-
 		return;
-
 	}
 
 	// Open the output file
-	ofstream outputFile(outputFilename);
+	ofstream outputFile("bundle.txt");
 
-	if (!outputFile) {
-
+	if (!outputFile)
+	{
 		cerr << "Error opening output file." << endl;
-
 		return;
 	}
 
 	// Decrypt the contents of the input file
 	char c;
 
-	while (inputFile.get(c)) {
-
-		c -= encryptionKey; // Subtract the encryption key from the ASCII code of each character to decrypt it
+	while (inputFile.get(c))
+	{
+		c -= 5; // Subtract the encryption key from the ASCII code of each character to decrypt it
 
 		outputFile.put(c); // Write the decrypted character to the output file
 	}
@@ -90,18 +63,100 @@ void decryptFile(const string& inputFilename, const string& outputFilename, int 
 	inputFile.close();
 	outputFile.close();
 
-	std::cout << "File decrypted successfully." << endl;
+	const char* filename = "encrypted_bundle.txt";
+
+	// Delete the old plain text file
+	if (remove(filename) != 0) {
+
+		cerr << "Error deleting file" << endl;
+
+	}
+
+	cout << "File decrypted successfully." << endl;
+
+}
+
+
+void encryptFile(const string& bundleFile) {
+
+	// Open the input file
+	ifstream inputFile(bundleFile);
+
+	if (!inputFile)
+	{
+		cerr << "Error opening input file." << endl;
+
+		return;
+	}
+
+	// Open the output file
+	ofstream outputFile("encrypted_bundle.txt");
+
+	if (!outputFile)
+	{
+		cerr << "Error opening output file." << endl;
+
+		return;
+	}
+
+	// Encrypt the contents of the input file
+	char c;
+	while (inputFile.get(c))
+	{
+		c += 5; // Add the encryption key to the ASCII code of each character
+
+		outputFile.put(c); // Write the encrypted character to the output file
+	}
+
+	// Close the files
+	inputFile.close();
+	outputFile.close();
+
+	const char* filename = "bundle.txt";
+
+	if (remove(filename) != 0) {
+
+		cerr << "Error deleting file" << endl;
+
+	}
+
+	cout << "File encrypted successfully." << endl;
+}
+
+void decryptFileMenu() {
+
+	string file;
+
+	cout << R"(
+
+     ____                             _   
+    |  _ \  ___  ___ _ __ _   _ _ __ | |_ 
+    | | | |/ _ \/ __| '__| | | | '_ \| __|
+    | |_| |  __/ (__| |  | |_| | |_) | |_
+    |____/ \___|\___|_|   \__, | .__/ \__|
+                          |___/|_|
+
+
+)";
+
+	// The user has to enter the file name that he wants to decrypt (encrypted_bundle.txt)
+	cout << "Enter file name: ";
+	cin >> file;
+
+	decryptFile(file);
 
 }
 
 // Hash Function
-unsigned int Hash(string& data) {
+unsigned int Hash(string& data)
+{
 
 	unsigned int result(0);
 
 
 	// for characters in data
-	for (unsigned int ch : data) {
+	for (unsigned int ch : data)
+	{
 
 		result = ch + (result << 4) + (result << 10) - result;
 
@@ -112,7 +167,8 @@ unsigned int Hash(string& data) {
 }
 
 // function for file creation
-void createUserFile(string a) {
+void createUserFile(string a)
+{
 
 	unsigned int hashUser{ Hash(a) };
 
@@ -123,14 +179,16 @@ void createUserFile(string a) {
 	MyFile << hashUser << endl;
 
 	// if statements for User file creation
-	if (MyFile.is_open()) {
+	if (MyFile.is_open())
+	{
 
-		cout << "User Created Succesfully!" << endl;
+		cout << "Username Selected\n\n" << endl;
 
 	}
-	else if (!MyFile.is_open()) {
+	else if (!MyFile.is_open())
+	{
 
-		cout << "Failed to create User" << endl;
+		cout << "Failed to create Username" << endl;
 
 	}
 
@@ -139,7 +197,8 @@ void createUserFile(string a) {
 
 }
 
-void createHashFile(string a) {
+void createHashFile(string a)
+{
 
 	unsigned int hashCredentials{ Hash(a) };
 
@@ -150,12 +209,14 @@ void createHashFile(string a) {
 	MyFile << hashCredentials << endl;
 
 	// if statements for password hash file creation
-	if (MyFile.is_open()) {
+	if (MyFile.is_open())
+	{
 
 		cout << "Password saved Succesfully!" << endl;
 
 	}
-	else if (!MyFile.is_open()) {
+	else if (!MyFile.is_open())
+	{
 
 		cout << "Failed to save password" << endl;
 
@@ -164,13 +225,9 @@ void createHashFile(string a) {
 	// close the file
 	MyFile.close();
 
-}
+	this_thread::sleep_for(chrono::milliseconds(3000));
 
-// Hash Validation 
-bool checkHash(string a, string b) {
-
-	return a == b;
-
+	system("cls");
 }
 
 void createBundle()
@@ -178,20 +235,31 @@ void createBundle()
 
 	bool BundleMenu = false;
 	int counter = 0;
-	string url, username, password;
+	string url, username, password, addMore;
 
-	// user states the apps for the bundle (maximum 5)
+	// Lets the user store passwords for a maximum of 5 websites
 
-	// Create Bundle File
-	ofstream MyFile("bundle.txt", fstream::app);
+	// Create Bundle File and appends any changes to it instead of overwriting it
+	ofstream userBundle("bundle.txt", fstream::app);
 
-	while(BundleMenu != true)
+	while (BundleMenu != true)
 	{
-
 
 		if (counter < 5)
 		{
-			
+
+			cout << R"(
+
+     ____                  _ _          
+    | __ ) _   _ _ __   __| | | ___     
+    |  _ \| | | | '_ \ / _` | |/ _ \    
+    | |_) | |_| | | | | (_| | |  __/    
+    |____/ \__,_|_| |_|\__,_|_|\___|    
+
+ 
+
+)";
+
 			cout << "Enter URL: ";
 			cin >> url;
 
@@ -202,19 +270,34 @@ void createBundle()
 			cin >> password;
 
 
-			// Save data to the file
-			MyFile << url << "\n";
-			MyFile << username << endl;
-			MyFile << password << "\n\n";
+			// Saves input data provided by the user to the file
+			userBundle << url << "\n";
+			userBundle << username << endl;
+			userBundle << password << "\n\n";
 
-			// if statements for User file creation
-			if (MyFile.is_open()) {
+			// Checks if the data was saved and then it asks the user if they want to save more passwords
+			if (userBundle.is_open()) {
 
-				cout << "Data Saved Succesfully!" << endl;
+				cout << "Data Saved Succesfully!\n\n" << endl;
 				counter++;
 
+				cout << "Do You wish to add another account? [Y]es/[N]o: ";
+				cin >> addMore;
+				if (addMore == "Y" || addMore == "y")
+				{
+					continue;
+				}
+				else if (addMore == "N" || addMore == "n")
+				{
+					counter = 5;
+				}
+				else
+				{
+					cout << "ERROR: Incorrect user input (Layer 8)...";
+				}
+
 			}
-			else if (!MyFile.is_open()) {
+			else if (!userBundle.is_open()) {
 
 				cout << "ERROR: Failed to save data..." << endl;
 
@@ -223,71 +306,99 @@ void createBundle()
 		}
 		else
 		{
-			// closes the file
-			MyFile.close();
+
+			// Closes the file
+			userBundle.close();
+
+			// Encrypts the bundle file
+			encryptFile("bundle.txt");
+
+			// Exit the loop
 			BundleMenu = true;
+
 		}
-		
+		system("cls");
+
+		cout << R"(
 
 
 
-		// user states his passwords for each app (gives url as well) 
+				Thank Your for Trusting SimpleSave Password Manager
 
-		// the tet file gets encrypted and the user is given an encryption key
 
-		// info gets added to a text file for user to download 
+
+
+)";
+
+		system("pause");
+
 	}
-
 }
 
-void loggedInMenu(string a) {
+void loggedInMenu()
+{
+	string options;
 
-	int options;
+	cout << R"(
+			  ____       _           _   
+			 / ___|  ___| | ___  ___| |_ 
+			 \___ \ / _ \ |/ _ \/ __| __|
+			  ___) |  __/ |  __/ (__| |_ 
+			 |____/ \___|_|\___|\___|\__|
+                       
 
-	cout << a << " Select from the options below" << endl;
 
-	cout << "1. Create Bundle" << endl;
-	cout << "2. Decrypt File" << endl;
+)";
 
-	cout << "Select: ";
+	printf("\t\t\t\x1B[31m[C]\033[0mreate Bundle");
+	printf("\t\t\x1B[31m[D]\033[0mecrypt");
+	printf("\t\t\x1B[31m[E]\033[0mxit\n\n");
+
+	cout << "Select an option: ";
 	cin >> options;
 
-	switch (options) {
-
-	case 1:
-
+	if (options == "C" || options == "c")
+	{
 		system("cls");
-
 		createBundle();
 
-		break;
+	}
+	else if (options == "D" || options == "d")
+	{
+		system("cls");
+		decryptFileMenu();
 
-	case 2:
+	}
+	else if (options == "E" || options == "e") {
 
 		system("cls");
-
-		break;
+		exitScreen();
 
 	}
 
-}
-
-// Exit screen
-void exitScreen() {
-
-	system("cls");
-
-	cout << "Goodbye";
 
 }
 
 // Login menu
-void loginMenu() {
+void loginMenu()
+{
 
 	string user;
 	string pass;
 	string userFileLines;
 	string credentialsFileLines;
+
+	cout << R"(
+
+     _                _          
+    | |    ___   __ _(_)_ __     
+    | |   / _ \ / _` | | '_ \    
+    | |__| (_) | (_| | | | | |   
+    |_____\___/ \__, |_|_| |_|   
+                |___/            
+
+
+)";
 
 	cout << "Username: ";
 	cin >> user;
@@ -301,13 +412,16 @@ void loginMenu() {
 	// Check if the username entered by the user exists in the file
 	ifstream MyUsersFile("users.txt");
 
-	if (MyUsersFile.is_open()) {
+	if (MyUsersFile.is_open())
+	{
 
-		while (getline(MyUsersFile, userFileLines)) {
+		while (getline(MyUsersFile, userFileLines))
+		{
 
-			if (userFileLines.find(userHashString) != string::npos ) {
+			if (userFileLines.find(userHashString) != string::npos)
+			{
 
-				cout << "User found in SimpleSave Database" << endl;
+				cout << "Username exists...\n\n" << endl;
 
 				break;
 
@@ -316,7 +430,8 @@ void loginMenu() {
 		}
 		MyUsersFile.close();
 	}
-	else {
+	else
+	{
 
 		cout << "Unable to open file" << endl;
 
@@ -331,27 +446,27 @@ void loginMenu() {
 	string credentialsHashString = to_string(passHash);
 
 	ifstream MyCredentialsFile("credentials.txt");
-	
-	if (MyCredentialsFile.is_open()) {
 
-		while (getline(MyCredentialsFile, credentialsFileLines)) {
+	if (MyCredentialsFile.is_open())
+	{
 
-			if (credentialsFileLines.find(credentialsHashString) != string::npos ) {
+		while (getline(MyCredentialsFile, credentialsFileLines))
+		{
 
-				cout << "Welcome " << user << endl;
-
-				this_thread::sleep_for(chrono::milliseconds(3000));
+			if (credentialsFileLines.find(credentialsHashString) != string::npos)
+			{
 
 				system("cls");
 
-				loggedInMenu(user);
+				loggedInMenu();
 
 			}
 
 		}
 		MyCredentialsFile.close();
 	}
-	else if (getline(MyCredentialsFile, credentialsFileLines)) {
+	else if (getline(MyCredentialsFile, credentialsFileLines))
+	{
 
 		cout << "Oh oh... " << endl;
 		cout << "It seems you credentials do not match, try again." << endl;
@@ -364,7 +479,20 @@ void loginMenu() {
 }
 
 //account creation menu
-void registerMenu() {
+void registerMenu()
+{
+
+	cout << R"(
+
+      ____            _     _              
+     |  _ \ ___  __ _(_)___| |_ ___ _ __    
+     | |_) / _ \/ _` | / __| __/ _ \ '__|   
+     |  _ <  __/ (_| | \__ \ ||  __/ |      
+     |_| \_\___|\__, |_|___/\__\___|_|      
+                |___/                       
+
+)";
+
 
 	string user, pass;
 
@@ -376,75 +504,87 @@ void registerMenu() {
 
 	bool masterValid = false;
 
-	while (masterValid == false) {
+	while (masterValid == false)
+	{
 
 		cout << "Password: ";
 		cin >> pass;
 
-		if (pass.length() < 8) {
+		if (pass.length() < 8)
+		{
 
 			system("cls");
 
 			cout << "Passwords must be 8 characters or more" << "\n\n";
 
 		}
-
-		else if (pass.length() >= 8) {
+		else if (pass.length() >= 8)
+		{
 
 			masterValid = true;
-
 			createHashFile(pass);
 
 		}
 
 	}
 
-	int select;
+	string options;
 
-	cout << "1. Login" << endl;
-	cout << "2. EXIT" << endl;
-	cout << "\nSelect: ";
-	cin >> select;
+	cout << R"(
 
-	switch (select) {
+			  ____  _                 _        ____                  
+			 / ___|(_)_ __ ___  _ __ | | ___  / ___|  __ ___   _____ 
+			 \___ \| | '_ ` _ \| '_ \| |/ _ \ \___ \ / _` \ \ / / _ \
+			  ___) | | | | | | | |_) | |  __/  ___) | (_| |\ V /  __/
+			 |____/|_|_| |_| |_| .__/|_|\___| |____/ \__,_| \_/ \___|
+                                           |_|
 
-	case 1:
+)";
 
+	printf("\t\t\t\t\t\x1B[31m[L]\033[0mogin");
+	printf("\t\t\x1B[31m[E]\033[0mxit\n\n");
+
+	cout << "Select an option: ";
+	cin >> options;
+
+	if (options == "L" || options == "l")
+	{
 		system("cls");
-
 		loginMenu();
 
-		break;
-
-	case 2:
-
+	}
+	else if (options == "E" || options == "e")
+	{
 		system("cls");
-
 		exitScreen();
 
 	}
 
 }
 
-void getOneTimePass() {
+void getOneTimePass()
+{
 
 	int passLen;
 	bool validLen = false;
 
-	while (validLen == false) {
+	while (validLen == false)
+	{
 
 		cout << "Enter the length of the password: ";
 		cin >> passLen;
 
-		if (passLen < 8 || passLen > 16) {
-
+		// Checks if the user's chosen password length is above 8 but below 64
+		// THIS DOES NOT SAVE IT PERMANENTLY THE USER HAS TO COPY AND PASTE IT
+		if (passLen < 8 || passLen > 64)
+		{
 			system("cls");
 
-			cout << "Remember, the length should be above 8 characters but below 16." << "\n\n";
-
+			cout << "Remember, the length should be above 8 characters but below 64." << "\n\n";
 		}
 
-		else if (passLen >= 8) {
+		else if (passLen >= 8)
+		{
 
 			validLen = true;
 
@@ -452,14 +592,15 @@ void getOneTimePass() {
 
 	}
 
-	const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#@";
+	const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=/;,.\"\'\'\\";
 	const int charsLength = chars.length();
 
 	string randomPass = "";
 
 	srand(time(nullptr));
 
-	for (int i = 0; i <= passLen; i++) {
+	for (int i = 0; i <= passLen; i++)
+	{
 
 		randomPass += chars[rand() % charsLength];
 
@@ -467,63 +608,73 @@ void getOneTimePass() {
 
 	system("cls");
 
-	cout << "Your " << passLen << " character password is: " << randomPass;
+	cout << "Your " << passLen << " length, One Time Password is: " << randomPass << "\n\n";
 
 }
 
-void mainMenu() {
+// Main Menu
+void mainMenu()
+{
 
-	int options;
+	string options;
 
-	cout << "1. Login" << endl;
-	cout << "2. New to SimpleSave?, create an account!" << endl;
-	cout << "3. Get Strong one time Password" << endl;
-	cout << "4. EXIT" << "\n\n";
-	cout << "Select: ";
+	cout << R"(
 
+			  ____  _                 _        ____                  
+			 / ___|(_)_ __ ___  _ __ | | ___  / ___|  __ ___   _____ 
+			 \___ \| | '_ ` _ \| '_ \| |/ _ \ \___ \ / _` \ \ / / _ \
+			  ___) | | | | | | | |_) | |  __/  ___) | (_| |\ V /  __/
+			 |____/|_|_| |_| |_| .__/|_|\___| |____/ \__,_| \_/ \___|
+                                           |_|
+
+	Welcome to SimpleSave a simple way to save your passwords in a secure environment!
+
+)";
+
+	// Menu options with custom colors
+	printf("\t\t\x1B[31m[L]\033[0mogin");
+	printf("\t\t\x1B[31m[C]\033[0mreate Account");
+	printf("\t\x1B[31m[G]\033[0menerate OTP");
+	printf("\t\t\x1B[31m[E]\033[0mxit\n\n");
+
+	cout << "Select an option: ";
 	cin >> options;
 
-	switch (options) {
 
-	case 1:
-
+	if (options == "L" || options == "l")
+	{
 		system("cls");
 
 		loginMenu();
 
-		break;
-
-	case 2:
-
+	}
+	else if (options == "C" || options == "c")
+	{
 		system("cls");
 
 		registerMenu();
 
-		break;
-
-	case 3:
-
+	}
+	else if (options == "G" || options == "g")
+	{
 		system("cls");
 
 		getOneTimePass();
 
-		break;
-
-
-	case 4:
-
+	}
+	else if (options == "E" || options == "e")
+	{
 		system("cls");
 
 		exitScreen();
-
-		break;
 
 	}
 
 }
 
-// main function
-int main() {
+// Main function
+int main()
+{
 
 	mainMenu();
 
